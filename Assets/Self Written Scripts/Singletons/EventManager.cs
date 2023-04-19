1f43e1b15e889e9b913dba9b0ae8df04ac1a3ca7
sarah <sarahapplebaum.sia@gmail.com>
2023-04-19 19:46:38 -0400

    more work

--- Assets/Self Written Scripts/Singletons/EventManager.cs
+++ Assets/Self Written Scripts/Singletons/EventManager.cs
@@ -123,6 +123,7 @@ public class EventManager : Singleton<EventManager>
     }
 
     public void ChangeCameraPosition(Transform newPos){
+        Debug.Log("changing camera pos to "+newPos.name);
         gameCamera.transform.position = newPos.position;
     }
 
